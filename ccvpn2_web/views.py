d210b392aef19f351916feabacb7566ec9964580
0xa <xa@xomg.net>
2013-07-26 01:51:08 +0200

    Add pages

--- ccvpn2_web/views.py
+++ ccvpn2_web/views.py
@@ -1,13 +1,23 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 from .models import User, Order
+from pyramid.httpexceptions import HTTPNotFound
+import markdown
+import os
 
 @view_config(route_name='home', renderer='ccvpn2_web:templates/home.mako')
-def my_view(request):
+def home(request):
     return {}
 
-
-
+@view_config(route_name='page', renderer='ccvpn2_web:templates/page.mako')
+def page(request):
+    path = 'ccvpn2_web/pages/'+request.matchdict['page']+'.md'
+    try:
+        f = open(path, 'r')
+        content = markdown.markdown(f.read())
+        return {'content':content}
+    except FileNotFoundError:
+        return HTTPNotFound()
 
 
 
