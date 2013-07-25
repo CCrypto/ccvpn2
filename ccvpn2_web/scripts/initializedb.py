670f1647e86d4ca4ee8e2176b0d11c97b4541435
0xa <xa@xomg.net>
2013-07-25 18:01:06 +0200

    moar stuff

--- ccvpn2_web/scripts/initializedb.py
+++ ccvpn2_web/scripts/initializedb.py
@@ -9,11 +9,7 @@ from pyramid.paster import (
     setup_logging,
     )
 
-from ..models import (
-    DBSession,
-    MyModel,
-    Base,
-    )
+from ..models import DBSession, Base
 
 
 def usage(argv):
@@ -32,6 +28,6 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-    with transaction.manager:
-        model = MyModel(name='one', value=1)
-        DBSession.add(model)
+    #with transaction.manager:
+        #model = MyModel(name='one', value=1)
+        #DBSession.add(model)
--- ccvpn2_web/views.py
+++ ccvpn2_web/views.py
@@ -1,14 +1,14 @@
 from pyramid.response import Response
 from pyramid.view import view_config
+from .models import User, Order
 
-from sqlalchemy.exc import DBAPIError
-
-from .models import (
-    DBSession,
-    MyModel,
-    )
-
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='ccvpn2_web:templates/home.mako')
 def my_view(request):
-    return {'one': one, 'project': 'ccvpn2_web'}
+    return {}
+
+
+
+
+
+
 
