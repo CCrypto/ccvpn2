b2ce2e7db1dd8532fd18bb5fccdd15efe76ce9af
Alice <alice@packetimpact.net>
2013-10-22 22:52:27 +0200

    who reads changelog with git anyway

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
@@ -22,7 +21,7 @@ requires = [
 setup(name='ccvpn',
       version='0.0',
       description='ccvpn',
-      long_description=README + '\n\n' + CHANGES,
+      long_description=README,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
