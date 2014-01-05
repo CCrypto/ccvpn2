24ba4e00e16cb2eda4797575fe6a72f41665e108
0xa <xa@xomg.net>
2014-01-05 06:23:40 +0100

    Add Alembic

---
+++ alembic/versions/1deecd313c4_initial.py
@@ -0,0 +1,22 @@
+"""initial
+
+Revision ID: 1deecd313c4
+Revises: None
+Create Date: 2014-01-05 06:22:41.816331
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1deecd313c4'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
