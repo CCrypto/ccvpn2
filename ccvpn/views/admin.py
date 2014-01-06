bc305fd57d8e24083804bc5682c9492b3e2b39e4
0xa <xa@xomg.net>
2014-01-06 10:59:04 +0100

    mew

--- ccvpn/views/admin.py
+++ ccvpn/views/admin.py
@@ -12,9 +12,9 @@ def monthdelta(date, delta):
     y = date.year + (date.month + delta - 1) // 12
     if not m:
         m = 12
-    e = [28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     d = min(date.day, [31, 29 if y % 4 == 0 and not y % 400 == 0
-                       else e][m - 1])
+                       else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+                       ][m - 1])
     return date.replace(day=d, month=m, year=y)
 
 
