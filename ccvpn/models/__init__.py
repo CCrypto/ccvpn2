c11530ace28a6b265083fb14c995e1f21fe4ec6f
Alice <alice@packetimpact.net>
2014-12-11 22:29:35 +0100

    i should write tests again.

--- ccvpn/models/__init__.py
+++ ccvpn/models/__init__.py
@@ -6,6 +6,7 @@ from .sql import User, PasswordResetToken, Profile
 from .sql import GiftCode, AlreadyUsedGiftCode
 from .sql import Order, OrderNotPaid, Gateway, VPNSession
 from .sql import Ticket, TicketMessage
+from .sql import random_access_token, random_gift_code, random_bytes
 
 from .icinga import IcingaQuery, IcingaError
 
